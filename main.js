35af91e9211704bbcfe4cf67836fe8b5e8c1a719
CamilaFlorez12 <07camilaflorezsantos@gmail.com>
2025-06-22 17:48:00 -0500

    fix: :bug: fix function
    fixed problems with some functions
    
    By @CamilaFlorez

--- main.js
+++ main.js
@@ -4,8 +4,10 @@ async function cargarProductos() {
     const respuesta = await fetch("https://fakestoreapi.com/products");
 
     const productos = await respuesta.json();
-    products = productos
-    renderProducts(products);
+    products = productos;
+    if (!document.body.classList.contains("category-page")) {
+  renderProducts(products);
+}
   } catch (error) {
     console.log("Ocurrió un error al cargar los productos", error)
   }
@@ -38,7 +40,13 @@ function renderProducts(lista) {
   );agregarfuncionboton();
 }
 
-
+function actualizarContador(){
+  const car=JSON.parse(localStorage.getItem("car"))||[];
+  const contador=document.querySelector(".contador");
+  if(contador){
+    contador.textContent=car.length;
+  }
+}
   function agregarfuncionboton(){document.querySelectorAll(".add-to-cart").forEach(button => {
     button.addEventListener("click", () => {
       const id = parseInt(button.getAttribute("data-id"));
@@ -46,6 +54,7 @@ function renderProducts(lista) {
       let car = JSON.parse(localStorage.getItem("car")) || []
       car.push(product)
       localStorage.setItem("car", JSON.stringify(car));
+      actualizarContador();
       console.log("producto agregado:", product)
       console.log(JSON.parse(localStorage.getItem("car")))
     });
@@ -66,48 +75,77 @@ cargarProductos().then(() => {
 
   })
 });
-if (document.body.classList.contains("car-shopping")) {
+
+if(document.body.classList.contains("category-page")){
+    const filterProducts=JSON.parse(localStorage.getItem("filtered"))||[];
+    renderProducts(filterProducts);
+  }
+if (document.getElementById("car-container")) {
   const contenedor = document.getElementById("car-container");
   
-  const carrito = JSON.parse(localStorage.getItem("car")) || [];
-  console.log(carrito)
-  carrito.forEach(product => {
-    const tarjeta =document.createElement("div");
-    tarjeta.classList.add("tarjet")
+  function renderizarCarrito(){
+     const carrito = JSON.parse(localStorage.getItem("car")) || [];
+     contenedor.innerHTML="";
+      if (carrito.length === 0) {
+    contenedor.innerHTML = `<h2 style="text-align:center; color:white;">No hay Productos en el carrito</h2>`;
+    return;
+  }
+    carrito.forEach((product, index) => {
+    const tarjeta = document.createElement("div");
+    tarjeta.classList.add("tarjet");
     tarjeta.innerHTML = `
-    <img src="${product.image}" alt="${product.title}">
-    <h3 class="name-product">${product.title}</h3>
-    <p class="money-product">$${product.price}</p>
-    <button class="eliminate">Eliminate</button>
-    <button class="buy">Buy</button>
-
-  `
-  contenedor.appendChild(tarjeta);
+      <img src="${product.image}" alt="${product.title}">
+      <h3 class="name-product">${product.title}</h3>
+      <p class="money-product">$${product.price}</p>
+      <button class="eliminate" data-index="${index}">Eliminate</button>
+      <button class="buy" data-index="${index}">Buy</button>
+    `;
+    contenedor.appendChild(tarjeta);
   });
-    const buttonEliminate=document.querySelectorAll(".eliminate");
+  const buttonEliminate=document.querySelectorAll(".eliminate");
     buttonEliminate.forEach(button=>{
       button.addEventListener("click",()=>{
-        const index=parseInt(button.getAttribute("index"));
+        const index=parseInt(button.getAttribute("data-index"));
         const car=JSON.parse(localStorage.getItem("car"))||[];
         car.splice(index,1);
         localStorage.setItem("car",JSON.stringify(car));
-        location.reload();
+        renderizarCarrito();
+        actualizarContador();
       });
     });
     const buttonBuy=document.querySelectorAll(".buy");
     buttonBuy.forEach(button=>{
       button.addEventListener("click",()=>{
-        const index=parseInt(button.getAttribute("index"));
+        const index=parseInt(button.getAttribute("data-index"));
         const car=JSON.parse(localStorage.getItem("car"))||[];
         car.splice(index,1);
         localStorage.setItem("car",JSON.stringify(car));
-        location.reload();
+        renderizarCarrito();
+        actualizarContador();
         alert("Producto comprado");
-      })
-    })
+      });
+    });
   }
-  
-  
+
+   
+
+  const car=document.querySelector(".ri-shopping-cart-2-line");
+  const cloting=document.getElementById("car-container");
+  car.addEventListener("click",()=>{
+    if (cloting.style.display==="none"||cloting.style.display===""){
+      cloting.style.display="flex";
+      cloting.style.flexDirection="column"
+      renderizarCarrito();
+
+    }else{
+      cloting.style.display="none";
+      
+    }
+  });
+    
+  renderizarCarrito();
+}
+
 if (document.body.classList.contains("login-page")) {
   const clothes = document.getElementById("icon-clothes");
   const containerClothes = document.querySelector(".container-clothes");
@@ -124,26 +162,8 @@ if (document.body.classList.contains("login-page")) {
   function filterCategory(category) {
     const filter = products.filter(product =>
       product.category === category);
-    const container = document.getElementById("shopping");
-    container.innerHTML = "";
-    filter.forEach(product => {
-      const tarjeta = `
-       <div class="container">
-      <img src="${product.image}" alt="${product.title}" class="image-products">
-      <div class="list-products">
-        <h3 class="name-product">${product.title}</h3>
-        <p class="money-product">$${product.price}</p>
-        <div class="buy-car">
-        <button class="add-to-cart" data-id="${product.id}">
-        <i class="ri-shopping-cart-2-line"></i>
-        </button>
-        </div>
-        </div>
-        </div>
-      `;
-      
-      container.innerHTML += tarjeta;
-    })
+      localStorage.setItem("filtered",JSON.stringify(filter));
+      window.location.href="category.html";
     
   }
 
