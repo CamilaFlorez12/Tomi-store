0fd472034ef6baf3544650b9eca585de130aeed7
CamilaFlorez12 <07camilaflorezsantos@gmail.com>
2025-06-22 21:27:03 -0500

    fix: :bug: fixed page
    fixed page because is wasn't filtering
    
    By @CamilaFlorez

--- main.js
+++ main.js
@@ -6,18 +6,19 @@ async function cargarProductos() {
     const productos = await respuesta.json();
     products = productos;
     if (!document.body.classList.contains("category-page")) {
-  renderProducts(products);
-}
+      renderProducts(products);
+      console.log("Productos cargados:", products);
+    }
   } catch (error) {
-    console.log("Ocurrió un error al cargar los productos", error)
+    console.log("Ocurrió un error al cargar los productos", error);
   }
 }
 function renderProducts(lista) {
   const container = document.getElementById("shopping");
-  if(!container) return;
+  if (!container) return;
   container.innerHTML = "";
 
-  lista.forEach(product => {
+  lista.forEach((product) => {
     const tarjeta = document.createElement("div");
     tarjeta.className = "box";
     tarjeta.innerHTML = `
@@ -35,91 +36,94 @@ function renderProducts(lista) {
         </div>
       `;
     container.appendChild(tarjeta);
-    
-  }
-  );agregarfuncionboton();
+  });
+  agregarfuncionboton();
 }
 
-function actualizarContador(){
-  const car=JSON.parse(localStorage.getItem("car"))||[];
-  const contador=document.querySelector(".contador");
-  if(contador){
-    contador.textContent=car.length;
+function actualizarContador() {
+  const car = JSON.parse(localStorage.getItem("car")) || [];
+  const contador = document.querySelector(".contador");
+  if (contador) {
+    contador.textContent = car.length;
   }
 }
-  function agregarfuncionboton(){document.querySelectorAll(".add-to-cart").forEach(button => {
+function agregarfuncionboton() {
+  document.querySelectorAll(".add-to-cart").forEach((button) => {
     button.addEventListener("click", () => {
       const id = parseInt(button.getAttribute("data-id"));
-      const product = products.find(product => product.id === id);
-      let car = JSON.parse(localStorage.getItem("car")) || []
-      car.push(product)
+      const product = products.find((product) => product.id === id);
+      let car = JSON.parse(localStorage.getItem("car")) || [];
+      car.push(product);
       localStorage.setItem("car", JSON.stringify(car));
       actualizarContador();
-      console.log("producto agregado:", product)
-      console.log(JSON.parse(localStorage.getItem("car")))
+      console.log("producto agregado:", product);
+      console.log(JSON.parse(localStorage.getItem("car")));
     });
   });
 }
-
 
 cargarProductos().then(() => {
   const container = document.querySelectorAll(".container");
   container.forEach((item) => {
     item.addEventListener("mouseover", () => {
-      item.style.transform = 'scale(1.05)';
+      item.style.transform = "scale(1.05)";
       item.style.transition = "transform 0.3s ease";
     });
     item.addEventListener("mouseout", () => {
       item.style.transform = "scale(1)";
-    })
-
-  })
+    });
+  });
 });
 
-if(document.body.classList.contains("category-page")){
-    const filterProducts=JSON.parse(localStorage.getItem("filtered"))||[];
-    renderProducts(filterProducts);
-  }
+if (document.body.classList.contains("category-page")) {
+  const filtered = JSON.parse(localStorage.getItem("filtered")) || [];
+  products = filtered;
+  console.log("Productos filtrados:", products);
+  renderProducts(filtered);
+}
+
+
+
 if (document.getElementById("car-container")) {
   const contenedor = document.getElementById("car-container");
-  
-  function renderizarCarrito(){
-     const carrito = JSON.parse(localStorage.getItem("car")) || [];
-     contenedor.innerHTML="";
-      if (carrito.length === 0) {
-    contenedor.innerHTML = `<h2 style="text-align:center; color:white;">No hay Productos en el carrito</h2>`;
-    return;
-  }
+
+  function renderizarCarrito() {
+    const carrito = JSON.parse(localStorage.getItem("car")) || [];
+    contenedor.innerHTML = "";
+    if (carrito.length === 0) {
+      contenedor.innerHTML = `<h2 style="text-align:center; color:white;">No hay Productos en el carrito</h2>`;
+      return;
+    }
     carrito.forEach((product, index) => {
-    const tarjeta = document.createElement("div");
-    tarjeta.classList.add("tarjet");
-    tarjeta.innerHTML = `
+      const tarjeta = document.createElement("div");
+      tarjeta.classList.add("tarjet");
+      tarjeta.innerHTML = `
       <img src="${product.image}" alt="${product.title}">
       <h3 class="name-product">${product.title}</h3>
       <p class="money-product">$${product.price}</p>
       <button class="eliminate" data-index="${index}">Eliminate</button>
       <button class="buy" data-index="${index}">Buy</button>
     `;
-    contenedor.appendChild(tarjeta);
-  });
-  const buttonEliminate=document.querySelectorAll(".eliminate");
-    buttonEliminate.forEach(button=>{
-      button.addEventListener("click",()=>{
-        const index=parseInt(button.getAttribute("data-index"));
-        const car=JSON.parse(localStorage.getItem("car"))||[];
-        car.splice(index,1);
-        localStorage.setItem("car",JSON.stringify(car));
+      contenedor.appendChild(tarjeta);
+    });
+    const buttonEliminate = document.querySelectorAll(".eliminate");
+    buttonEliminate.forEach((button) => {
+      button.addEventListener("click", () => {
+        const index = parseInt(button.getAttribute("data-index"));
+        const car = JSON.parse(localStorage.getItem("car")) || [];
+        car.splice(index, 1);
+        localStorage.setItem("car", JSON.stringify(car));
         renderizarCarrito();
         actualizarContador();
       });
     });
-    const buttonBuy=document.querySelectorAll(".buy");
-    buttonBuy.forEach(button=>{
-      button.addEventListener("click",()=>{
-        const index=parseInt(button.getAttribute("data-index"));
-        const car=JSON.parse(localStorage.getItem("car"))||[];
-        car.splice(index,1);
-        localStorage.setItem("car",JSON.stringify(car));
+    const buttonBuy = document.querySelectorAll(".buy");
+    buttonBuy.forEach((button) => {
+      button.addEventListener("click", () => {
+        const index = parseInt(button.getAttribute("data-index"));
+        const car = JSON.parse(localStorage.getItem("car")) || [];
+        car.splice(index, 1);
+        localStorage.setItem("car", JSON.stringify(car));
         renderizarCarrito();
         actualizarContador();
         alert("Producto comprado");
@@ -127,54 +131,50 @@ if (document.getElementById("car-container")) {
     });
   }
 
-   
-
-  const car=document.querySelector(".ri-shopping-cart-2-line");
-  const cloting=document.getElementById("car-container");
-  car.addEventListener("click",()=>{
-    if (cloting.style.display==="none"||cloting.style.display===""){
-      cloting.style.display="flex";
-      cloting.style.flexDirection="column"
+  const car = document.querySelector(".ri-shopping-cart-2-line");
+  const cloting = document.getElementById("car-container");
+  car.addEventListener("click", () => {
+    if (cloting.style.display === "none" || cloting.style.display === "") {
+      cloting.style.display = "flex";
+      cloting.style.flexDirection = "column";
       renderizarCarrito();
-
-    }else{
-      cloting.style.display="none";
-      
+    } else {
+      cloting.style.display = "none";
     }
   });
-    
+
   renderizarCarrito();
 }
 
+document.addEventListener("DOMContentLoaded", () => {
 if (document.body.classList.contains("login-page")) {
   const clothes = document.getElementById("icon-clothes");
   const containerClothes = document.querySelector(".container-clothes");
-  console.log(clothes)
+  console.log(clothes);
   clothes.addEventListener("click", () => {
     if (containerClothes.style.display === "none") {
       containerClothes.style.display = "flex";
     } else {
       containerClothes.style.display = "none";
     }
-  }
-  )
+  });
+
 
   function filterCategory(category) {
-    const filter = products.filter(product =>
-      product.category === category);
-      localStorage.setItem("filtered",JSON.stringify(filter));
-      window.location.href="category.html";
-    
+    const filter = products.filter((product) => product.category === category);
+    localStorage.setItem("filtered", JSON.stringify(filter));
+    window.location.href = "category.html";
   }
 
-const searchbar=document.querySelector(".search__input");
-searchbar.addEventListener("input",()=>{  const valueInput=searchbar.value.toLowerCase();
-    document.getElementById("shopping").innerHTML="";
-    products.forEach(product=>{
-      if(product.title.toLowerCase().includes(valueInput)){
-          const tarjeta = document.createElement("div");
-    tarjeta.className = "box";
-    tarjeta.innerHTML = `
+  const searchbar = document.querySelector(".search__input");
+  searchbar.addEventListener("input", () => {
+    const valueInput = searchbar.value.toLowerCase();
+    document.getElementById("shopping").innerHTML = "";
+    products.forEach((product) => {
+      if (product.title.toLowerCase().includes(valueInput)) {
+        const tarjeta = document.createElement("div");
+        tarjeta.className = "box";
+        tarjeta.innerHTML = `
       <div class="container">
       <img src="${product.image}" alt="${product.title}" class="image-products">
       <div class="list-products">
@@ -188,36 +188,38 @@ searchbar.addEventListener("input",()=>{  const valueInput=searchbar.value.toLow
         </div>
         </div>
       `;
-    document.getElementById("shopping").appendChild(tarjeta)
+        document.getElementById("shopping").appendChild(tarjeta);
       }
-    })})
- 
+    });
+  });
 
   const buttonWomen = document.getElementById("button-women");
   const buttonMen = document.getElementById("button-men");
   const buttonjewelery = document.getElementById("button-jewelery");
-  const buttonElectronics = document.getElementById("button-electronics")
+  const buttonElectronics = document.getElementById("button-electronics");
   buttonWomen.addEventListener("click", () => {
     filterCategory("women's clothing");
-     agregarfuncionboton();
+    agregarfuncionboton();
   });
   buttonMen.addEventListener("click", () => {
     filterCategory("men's clothing");
-     agregarfuncionboton();
-  })
+    agregarfuncionboton();
+  });
+
   buttonjewelery.addEventListener("click", () => {
     filterCategory("jewelery");
-     agregarfuncionboton();
-  })
+    agregarfuncionboton();
+  });
   buttonElectronics.addEventListener("click", () => {
-    filterCategory("electronics"); agregarfuncionboton();
-  })
+    filterCategory("electronics");
+    agregarfuncionboton();
+  });
 
   function VewProducts(products) {
     const filterPrice = products.sort((a, b) => a.price - b.price);
     const container = document.getElementById("shopping");
     container.innerHTML = "";
-    filterPrice.forEach(product => {
+    filterPrice.forEach((product) => {
       const tarjeta = `
       <div class="box">
           <div class="container">
@@ -225,18 +227,18 @@ searchbar.addEventListener("input",()=>{  const valueInput=searchbar.value.toLow
             <div class="list-products">
               <h3 class="name-product">${product.title}</h3>
               <p class="money-product">$${product.price}</p>
-              <div class="buy-car>
+              <div class="buy-car">
                 <a href="#car-shopping"><button><i class="ri-shopping-cart-2-line"></i></button></a>
               </div>
             </div>
           </div>
           </div>
-    `
+    `;
       container.innerHTML += tarjeta;
-    })
-  };
+    });
+  }
 }
-
+})
 // const buttonPrice = document.getElementById("price");
 // buttonPrice.addEventListener("click", () => {
 //   VewProducts(products);
